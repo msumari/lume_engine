875bf6657089e0c60dbe3785c98c060a61fd7ee9
msumari <suleykhery@gmail.com>
2021-08-26 11:27:06 +0300

    build:configuring and connecting MongoDB cloud

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
 
 app.listen(8800, () => {
   console.log("backend is up");
