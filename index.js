f0ebd2f075f942ec283d6dd407cb3b3209f89d94
msumari <suleykhery@gmail.com>
2021-08-30 18:48:01 +0300

    fix:fixing the staged commit for pulling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
 
 const userRoute = require("./routes/user");
 dotenv.config();
@@ -12,18 +12,15 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
   })
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
+app.use(express.json());
 
-  app.use(express.json());
-
-  app.use("/api/auth",authRoute);
+app.use("/api/auth", authRoute);
 
 app.use("/api/users", userRoute);
-
 
 app.listen(8800, () => {
   console.log("backend is up");
