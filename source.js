7d98214f225cf9a412295aae092f479db09f298d
msumari <suleykhery@gmail.com>
2021-11-08 15:14:11 +0300

    feat:Created movie model object from yts data

--- source.js
+++ source.js
@@ -12,7 +12,20 @@ async function getData(page, limit) {
     const response = await axios(config);
     let result = response.data.data.movies;
     for (let i = 0; i < result.length; i++) {
-      console.log(result[i].title);
+      let movie = {
+        title: "" + result[i].title,
+        desc: "" + result[i].description_full,
+        image: "" + result[i].background_image_original,
+        imageSm: "" + result[i].medium_cover_image,
+        video: "" + result[i].yt_trailer_code,
+        year: "" + result[i].year,
+        limit: result[i].runtime,
+        genre: result[i].genres,
+        limit: result[i].runtime,
+        language: "" + result[i].language,
+        rating: result[i].rating,
+      };
+      console.log(movie);
     }
   } else {
     return "parameters are required";
