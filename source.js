55e680a7ae990a988963c646b1ea6d4e5e31f945
msumari <suleykhery@gmail.com>
2021-11-09 11:35:24 +0300

    feat:Completed data migration script

--- source.js
+++ source.js
@@ -8,11 +8,12 @@ async function getData(page, limit) {
       Cookie: "PHPSESSID=s8tcefbvcicrtgqh5pqrfmnesj",
     },
   };
+
   if (page && limit) {
     const response = await axios(config);
     let result = response.data.data.movies;
     for (let i = 0; i < result.length; i++) {
-      let movie = {
+      let movie = JSON.stringify({
         title: "" + result[i].title,
         desc: "" + result[i].description_full,
         image: "" + result[i].background_image_original,
@@ -24,9 +25,27 @@ async function getData(page, limit) {
         limit: result[i].runtime,
         language: "" + result[i].language,
         rating: result[i].rating,
+      });
+
+      const config2 = {
+        method: "post",
+        url: "http://lume-engine.herokuapp.com/api/movie",
+        headers: {
+          token:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxODAwNGJiZTZlNGU1NmZhN2UwNzE4NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjM3NTE3NywiZXhwIjoxNjM2ODA3MTc3fQ.KmsoTOJOuyrY1L6cRO_kqT6cWsXZuvWT4pNJusc8TFc",
+          "Content-Type": "application/json",
+        },
+        data: movie,
       };
-      console.log(movie);
+      await axios(config2)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
+    console.log("complete");
   } else {
     return "parameters are required";
   }
